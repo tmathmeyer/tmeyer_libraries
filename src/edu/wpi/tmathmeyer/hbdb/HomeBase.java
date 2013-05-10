c7e8b1ab4b20528ba71c9988fca7cfe0a871dbcc
tmathmeyer <tmathmeyer@gmail.com>
2013-05-09 23:38:57 -0400

    removed a warning

--- src/edu/wpi/tmathmeyer/hbdb/HomeBase.java
+++ src/edu/wpi/tmathmeyer/hbdb/HomeBase.java
@@ -3,7 +3,6 @@ package edu.wpi.tmathmeyer.hbdb;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
